259b77bbb9d76405bff9000c6faf904611b82247
Kevin Bulebush <kmbulebu@gmail.com>
2014-07-26 00:31:41 -0400

    Added Arm/Disarm and Command Output write commands.

---
+++ src/main/java/com/oakcity/dsc/it100/commands/write/PartitionDisarmCommand.java
@@ -0,0 +1,22 @@
+package com.oakcity.dsc.it100.commands.write;
+
+import com.oakcity.dsc.it100.commands.ICommandHelp;
+
+public class PartitionDisarmCommand extends BasePartitionPlusCodeWriteCommand implements ICommandHelp {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3447190568071046840L;
+	
+	public static final String CODE = "040";
+
+	public PartitionDisarmCommand(int partition, String code) {
+		super(CODE, partition, code);
+	}
+
+	public String getDescription() {
+		return "Disarms the selected partition. Sending the Partition Disarm command will silence any alarms as well as disarm the partition.";
+	}
+
+}
